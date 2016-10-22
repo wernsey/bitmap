309ddd50831b0f2f8835d584d3ad0d309731c789
wstoop@gmail.com <wstoop@gmail.com>
2016-10-22 18:04:14 +0200

    Minor modifications to the test programs

--- misc/pbm.c
+++ misc/pbm.c
@@ -7,31 +7,31 @@ http://en.wikipedia.org/wiki/Netpbm_format
 #include <string.h>
 #include <ctype.h>
 
-#include "bmp.h"
+#include "../bmp.h"
 
 static char *readfile(const char *fname) {
-	FILE *f;
-	long len,r;
-	char *str;
-	
-	if(!(f = fopen(fname, "rb")))	
-		return NULL;
-	
-	fseek(f, 0, SEEK_END);
-	len = ftell(f);
-	rewind(f);
-	
-	if(!(str = malloc(len+2)))
-		return NULL;	
-	r = fread(str, 1, len, f);
-	
-	if(r != len) {
-		free(str);
-		return NULL;
-	}	
-	fclose(f);	
-	str[len] = '\0';
-	return str;
+    FILE *f;
+    long len,r;
+    char *str;
+
+    if(!(f = fopen(fname, "rb")))
+        return NULL;
+
+    fseek(f, 0, SEEK_END);
+    len = ftell(f);
+    rewind(f);
+
+    if(!(str = malloc(len+2)))
+        return NULL;
+    r = fread(str, 1, len, f);
+
+    if(r != len) {
+        free(str);
+        return NULL;
+    }
+    fclose(f);
+    str[len] = '\0';
+    return str;
 }
 
 static char *tokenize_pbm(char *s, char **r) {
@@ -39,7 +39,7 @@ static char *tokenize_pbm(char *s, char **r) {
 
     if(!s && r)
         p = *r;
-        
+
     if(!p) return NULL;
 
 start:
@@ -55,18 +55,18 @@ start:
         }
         goto start;
     }
-    
+
     for(e = p; *e && !isspace(*e); e++);
-    
+
     if(r) {
         if(*e != '\0')
             *r = e + 1;
         else
             *r = e;
     }
-    
+
     *e = '\0';
-    
+
     return p;
 }
 
@@ -94,35 +94,35 @@ struct bitmap *parse_pbm(const char *filename) {
         fprintf(stderr, "error: Invalid type %s\n", p);
         goto error;
     }
-        
+
 #define GET_INT(v, error_msg)   do{ if(!(p = tokenize_pbm(NULL, &r))) { \
                                     fprintf(stderr, "error: %s\n", error_msg); \
                                     goto error;\
                                 }\
                                 v = atoi(p); } while(0)
-    
+
     GET_INT(w, "Bad width");
     GET_INT(h, "Bad height");
-    
+
     if(type > 1) {
         GET_INT(d, "Bad depth");
     }
-    
+
     if(w <= 0 || h <= 0 || d <= 0) {
         fprintf(stderr, "error: invalid dimensions\n");
         goto error;
     }
-    
+
     printf("Creating bitmap %d x %d @ %d\n", w, h, d);
-    
+
     bm = bm_create(w,h);
-    
+
     for(y = 0; y < h; y++) {
         for(x = 0; x < w; x++) {
             int pr,pg,pb, c;
             switch(type) {
-                case 1: 
-                case 2: 
+                case 1:
+                case 2:
                     GET_INT(pr, "Bad value");
                     pr = pr * 255 / d;
                     pg = pr;
@@ -137,11 +137,11 @@ struct bitmap *parse_pbm(const char *filename) {
                     pb = pb * 255 / d;
                     break;
             }
-			c = 0xFF000000 | (pr << 16) | (pg << 8) | pb;
+            c = 0xFF000000 | (pr << 16) | (pg << 8) | pb;
             bm_set(bm, x, y, c);
         }
     }
-    
+
     goto done;
 error:
     if(bm) bm_free(bm);
@@ -156,13 +156,13 @@ int main(int argc, char *argv[]) {
     for(i = 1; i < argc; i++) {
         struct bitmap *b = parse_pbm(argv[i]);
         if(b) {
-            bm_save(b, "out.bmp");
-            bm_free(b); 
+            bm_save(b, "out.gif");
+            bm_free(b);
         } else {
             fprintf(stderr, "error: Unable to process %s\n", argv[i]);
         }
     }
-    
+
     return 0;
 }
 #endif
